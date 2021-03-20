f692e1d017df33ff31a80c398e7ac45f30f67f3b
Peter Pak <ppak10@gmail.com>
2021-03-20 17:38:47 -0500

    Updates graph component to generate graphs with provided values.

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -4,12 +4,13 @@
  */
 
 // Node Modules
-import {useContext, useCallback, useEffect, useRef, useState} from 'react';
+import {useContext, useEffect, useRef, useState} from 'react';
 
 // Constants
+const ANGLE_MOD = 0.25;
 const CIRCLE_RADIUS = 25;
-const LEVEL_RADIUS = 100;
-const LINE_WIDTH = 5;
+const LEVEL_RADIUS = 150;
+const LINE_WIDTH = 2.5;
 const FILL_TEXT_OFFSET = 10;
 const TWO_PI = 2 * Math.PI
 
@@ -23,18 +24,6 @@ export default function Graph() {
   const [height, setHeight] = useState(100);
   const [width, setWidth] = useState(100);
 
-  const handleCreateCircle = useCallback((ctx, x, y, busNumber) => {
-    ctx.beginPath();
-    ctx.arc(x, y, CIRCLE_RADIUS, 0, TWO_PI, false);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.lineWidth = LINE_WIDTH;
-    ctx.strokeStyle = '#000000';
-    ctx.stroke();
-    ctx.fillStyle = 'black'
-    ctx.fillText(busNumber, x - FILL_TEXT_OFFSET, y);
-  }, []);
-
   useEffect(() => {
     // Updates height and width of canvas element to bounding client rectangle.
     const current = ref.current;
@@ -47,27 +36,92 @@ export default function Graph() {
 
   useEffect(() => {
     const current = ref.current;
-    if (current && state.busNumber) {
+
+    if (current) {
+      // Creates blank layout for canvas.
+      const centerX = width / 2;
+      const centerY = height / 2;
       const ctx = current.getContext('2d');
       ctx.fillStyle = 'white';
       ctx.fillRect(0, 0, width, height);
-      const centerX = width / 2;
-      const centerY = height / 2;
-      handleCreateCircle(ctx, centerX, centerY, state.busNumber);
+      const nodeMap = new Map();
 
-      const busNumbersSet = state.edgeMap.get(parseInt(state.busNumber));
+      const handleDrawCircle = (x, y, busNumber) => {
+        ctx.beginPath();
+        ctx.arc(x, y, CIRCLE_RADIUS, 0, TWO_PI, false);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.lineWidth = LINE_WIDTH;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+        ctx.fillStyle = 'white'
+        ctx.fillText(busNumber, x - FILL_TEXT_OFFSET, y);
+      };
 
-      if (busNumbersSet) {
-        Array.from(busNumbersSet).forEach((number, index) => {
-          // Determines angle in radians for graph node.
-          const angle = TWO_PI * (index + 1) / busNumbersSet.size;
-          const x = centerX + Math.cos(angle) * LEVEL_RADIUS;
-          const y = centerY + Math.sin(angle) * LEVEL_RADIUS;
-          handleCreateCircle(ctx, x, y, number);
-        });
+      const handleDrawEdge = (startCoordinates, endCoordinates) => {
+        ctx.beginPath();
+        ctx.moveTo(startCoordinates.x, startCoordinates.y);
+        ctx.lineTo(endCoordinates.x, endCoordinates.y);
+        ctx.lineWidth = LINE_WIDTH;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+      };
+
+      const handleCreateNodes = (busNumber, level = 0, parentIndex = 0) => {
+        const busNumbersSet = state.edgeMap.get(parseInt(busNumber));
+
+        if (level < state.levels && busNumbersSet) {
+          Array.from(busNumbersSet).forEach((number, index) => {
+            // Determines angle in radians for graph node.
+            const angle = TWO_PI * (index + 1) / busNumbersSet.size;
+
+            // Adjust the angle of the graph node depending on current level and
+            // parentIndex values in order to prevent two nodes from being
+            // rendered directly on top of each other.
+            const adjustedAngle = angle + (level + parentIndex) * ANGLE_MOD;
+
+            // Implements positions using polar coordinates and converts to
+            // cartesian.
+            let coordinates = {
+              x: centerX + Math.cos(adjustedAngle) * LEVEL_RADIUS * (level + 1),
+              y: centerY + Math.sin(adjustedAngle) * LEVEL_RADIUS * (level + 1),
+            };
+
+            if (!nodeMap.has(parseInt(number))) {
+              // If node map does not have current bus number, it adds it to the
+              // set with calculated coordinates.
+              nodeMap.set(parseInt(number), coordinates);
+            } else {
+              // If node map already has the  current bus number, it uses the
+              // values that were already stored in map.
+              coordinates = nodeMap.get(parseInt(number));
+            }
+
+            // Draws the edge connection between the two nodes.
+            handleDrawEdge(nodeMap.get(parseInt(busNumber)), coordinates);
+
+            // Recursively creates related nodes.
+            // Passes current index down for parent index argument.
+            // (Intended to prevent nodes form overlapping.)
+            handleCreateNodes(number, level + 1, index);
+          });
+        }
+      };
+
+      if (state.busNumber) {
+        // Sets bus number and coordinates to node map.
+        nodeMap.set(parseInt(state.busNumber), {x: centerX, y: centerY});
+
+        // Creates nodes for edge endpoints.
+        handleCreateNodes(state.busNumber);
+
+        // Draws circles last to draw over edges.
+        for (let [number, {x, y}] of nodeMap.entries()) {
+          handleDrawCircle(x, y, number);
+        }
       }
-    }
-  }, [height, width, handleCreateCircle, state.busNumber, state.edgeMap]);
+    } 
+  }, [height, width, state.busNumber, state.edgeMap, state.levels]);
 
   return (
     <canvas ref={ref} height={height} width={width} />
